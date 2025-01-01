012f0755d533d8112f4021806a76494e899b2925
Adam Davis <a.davis.dev@gmail.com>
2025-01-01 12:45:19 -0500

    feat(app): add error page

---
+++ src/app/error.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Error() {
+    return <div className="flex items-center justify-center min-h-screen">
+        <div className="text-center">
+            <h1 className="text-4xl font-bold ">Uh Oh</h1>
+            <p className="mt-4 text-lg">Something went wrong ☹️</p>
+            <p className="text-xs mt-4">BHI Weather Report</p>
+        </div>
+    </div>
+}
